89f56771ea9e0db24ba8b8d17a0118c2b67350ba
unknown <jannathsyeda7@gmail.com>
2021-05-03 15:44:22 +0600

    home,directory and menu Item components and styling scss complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/HomePage/home-component';
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+    <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage/home-component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from './../../Components/Directory/Directory.component';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+         <Directory/>
+      </div>
+    );
+};
+
+export default HomePage;
